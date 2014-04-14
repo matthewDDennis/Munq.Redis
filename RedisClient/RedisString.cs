face3dab049ba65093d5db6759d66435bfac0b1c
Matthew <Matthew.Dennis@Rogers.com>
2014-04-13 22:34:17 -0400

    Added RedisString

---
+++ RedisClient/RedisString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Munq.Redis
+{
+    public class RedisString
+    {
+        public RedisString(RedisClient client, string key)
+        {
+            Client = client;
+            Key = key;
+        }
+
+        public RedisClient Client { get; private set; }
+        public string Key { get; private set; }
+    }
+}
