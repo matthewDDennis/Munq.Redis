de6a1ee903bc57127d620c8da257f1ecce8e684d
Matthew <Matthew.Dennis@Rogers.com>
2014-10-18 22:24:02 -0400

    added missing file

---
+++ RedisClient/Responses/IntegerResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Munq.Redis.Responses
+{
+    public static class IntegerResponse
+    {
+        public static async Task<long> ExpectIntegerAsync(this RedisClient client)
+        {
+            object response = await client.ReadResponseAsync().ConfigureAwait(false);
+            if (response is long)
+                return (long)response;
+            else if (response is RedisErrorString)
+            {
+                RedisErrorString errorString = response as RedisErrorString;
+                throw new RedisErrorException(errorString.Message);
+            }
+            else
+                throw new RedisInvalidResponseType(typeof(RedisBulkString), response.GetType());
+        }
+
+        public static async Task<bool> ExpectBitAsync(this RedisClient client)
+        {
+            long response = await client.ExpectIntegerAsync().ConfigureAwait(false);
+            switch (response)
+            {
+                case 0: return false;
+                case 1: return true;
+                default: throw new RedisUnexpectedResponse("0 or 1", response.ToString());
+            }
+        }
+    }
+}
